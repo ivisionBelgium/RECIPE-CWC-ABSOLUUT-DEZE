b9afc7c236a25fa4c38c1e699d3b3920bde7be63
Koen Sas <koen@ivision.be>
2022-10-11 13:34:53 +0200

    Add Recipe Tabview

---
+++ RECIPE_LIST_DEZE/Views/RecipeTabView.swift
@@ -0,0 +1,41 @@
+//
+//  RecipeTabView.swift
+//  RECIPE_LIST_DEZE
+//
+//  Created by Koen Sas on 11/10/2022.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        
+        TabView {
+            
+            Text("Featured")
+                .tabItem{
+                    
+                    VStack{
+                        
+                        Image(systemName: "globe.europe.africa.fill")
+                        Text("Featured")
+                        
+                    }
+              
+            }
+            RecipeListView()
+                .tabItem{
+                    
+                    Image(systemName: "list.bullet.rectangle.portrait.fill")
+                    
+                }
+        }
+    
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
