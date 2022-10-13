876ac1a3eb030823280c0b71368686639c687818
Koen Sas <koen@ivision.be>
2022-10-13 18:53:33 +0200

    List element veranderd door forEach met daarrond een scrollview zodat we typisch het scroll dumpen kunnen zien LOOk lzayVStack gebruikt voor de render clipping off screen

--- RECIPE_LIST_DEZE/Views/RecipeListView.swift
+++ RECIPE_LIST_DEZE/Views/RecipeListView.swift
@@ -14,46 +14,72 @@ struct RecipeListView: View {
     
     var body: some View {
         
+        
+        
+        
         NavigationView {
-            List(vModel.pubRecipes){ r in
-            NavigationLink(destination: RecipeDetailView(newVarRecipe: r),
-                
-                           label: {
-                    HStack {
-                        
-                        Image(r.image)
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 50, height: 50)
-                            .clipped()
-                            .cornerRadius(5)
-                            .font(.title3)
-                        
-                        Text(r.name)
-                            .padding()
-                        
-                    }
-                    .navigationBarTitle("All Ingredients")
-                    
-                })
-                
-                       
-                   }
-        }
             
-          
+            VStack (alignment: .leading) {
                 
+                Text("ALL Recipes")
+                    .bold()
+                    .font(.largeTitle)
+                  
+                    .padding(.top,40)
+                
+                //.bold() als deze hier staat dan krijgen we een versie error
+                ScrollView {
                     
-             
-
-             
-            }
-      
+                    // lazyVstack rendert alleen de daadwerkelijke zichtbare scroll items beter voor de CPU
+                    LazyVStack(alignment: .leading) {
+                        ForEach(vModel.pubRecipes) { r in
+                            
+                            NavigationLink(destination:  RecipeDetailView(newVarRecipe: r),
+                                           
+                                           label: {
+                                HStack {
+                                    
+                                    Image(r.image)
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 50, height: 50)
+                                        .clipped()
+                                        .cornerRadius(5)
+                                        .font(.title3)
+                                    
+                                    Text(r.name)
+                                        .padding()
+                                    
+                                }
+                                .navigationBarHidden(true)
+                                
+                            })
+                            
+                            
+                            
+                        }
+                    }
+                }
+            } .padding()
+            
+        }
+        
+        .navigationBarHidden(true)
+        
+        
+        
+        
+        
+        
+        
     }
+    
+}
 
 
 struct RecipeViewList_Previews: PreviewProvider {
     static var previews: some View {
         RecipeListView()
+            .environmentObject(RecipeViewModel())
     }
 }
