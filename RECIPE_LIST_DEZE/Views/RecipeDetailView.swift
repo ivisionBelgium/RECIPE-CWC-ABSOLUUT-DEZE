c1c190e13b8460fa743f44e6d4476d3a3f3267c1
Koen Sas <koen@ivision.be>
2022-10-18 10:27:29 +0200

    terug normaal maar niet werkende recipe list

--- RECIPE_LIST_DEZE/Views/RecipeDetailView.swift
+++ RECIPE_LIST_DEZE/Views/RecipeDetailView.swift
@@ -1,4 +1,4 @@
-//
+
 //  RecipeDetailView.swift
 //  RECIPE_LIST_DEZE
 //
@@ -9,59 +9,59 @@ import SwiftUI
 
 
 struct RecipeDetailView: View {
-    
+
         @State var varSelectedServings = 2
-        
-     
+
+
         // recipe is een nieuwe variabele
         var newVarRecipe:Recipetje
-    
+
     var body: some View {
- 
-       
-        
+
+
+
         ScrollView {
-           
+
             // MARK: Image en picker
             Image(newVarRecipe.image)
                 .resizable()
                 .scaledToFit()
-          
-           
-            
+
+
+
             VStack(alignment: .leading) {
-                
+
                 VStack {
-                    
+
                     // MARK: Picker
                     Picker("ServingsNr", selection: $varSelectedServings) {
-                        
+
                         Text("2").tag(2)
                         Text("4").tag(4)
                         Text("6").tag(6)
                         Text("8").tag(8)
-                        
+
                     }
                     .pickerStyle(SegmentedPickerStyle())
                     // we kunnen enkel 1 parameter ingeven hier, rest blijft hetzelfde
                     .frame(width: 160)
-                   
-                    
-                    
+
+
+
                     // deze text geeft he geselecteerde door
-                
+
                 //0   deze actieve kut nul gaf compiler is unable to type check
                 }
                 .padding(.leading, 0)
                 .padding(.trailing)
-                
-                
-                
+
+
+
                 // MARK: Ingredients
                 Text("Ingredients")
                     .padding(.bottom)
                     .font(.title)
-                 
+
                 // vooraleer de json aanpassin g
 //                ForEach (newVarRecipe.ingredients, id: \.self) { item in
 //
@@ -70,41 +70,41 @@ struct RecipeDetailView: View {
 //                }
                 // \.self kan hier weg want lijst is IDENTIFIABLE
                 ForEach (newVarRecipe.ingredients) { item in
-       
+
                     Text("- " + RecipeViewModel.getPortions(ingredient: item, recipeServings: Recipetje.servings, targetServings: varSelectedServings + "- " + item.name))
-         
+
                 }
-                
+
                 // MARK: Directions
-                
+
                 Text("Directions")
                     .padding(.vertical).font(.title)
-                 
-                
+
+
                 ForEach (0..<newVarRecipe.directions.count, id: \.self) { index in
-                    
+
                     Text(String(index) + ". " + newVarRecipe.directions[index])
                         .padding(.bottom, 2.0)
-                     
-                    
+
+
                 }
-                
+
             }
             .padding(.leading)
-    
-            
+
+
         }
         .navigationBarTitle(newVarRecipe.name)
-        
-       
-      
+
+
+
     }
 }
 
 
 struct RecipeDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        
+
         let prevModel = RecipeViewModel()
         RecipeDetailView(newVarRecipe: prevModel.pubRecipes[0])
     }
--- RECIPE_LIST_DEZE/Views/RecipeListView.swift
+++ RECIPE_LIST_DEZE/Views/RecipeListView.swift
@@ -1,85 +1,162 @@
+
 //
-//  RecipeListView.swift
-//  RECIPE_LIST_DEZE
+//  ContentView.swift
+//  Recipe List App
 //
-//  Created by Koen Sas on 08/10/2022.
+//  Created by Christopher Ching on 2021-01-14.
 //
 
 import SwiftUI
 
 struct RecipeListView: View {
     
-    // opgelet reference naar RecipeViewModel zonder ()
-    @EnvironmentObject var vModel : RecipeViewModel
+    @EnvironmentObject var vModel:RecipeViewModel
     
     var body: some View {
-        
-        
-        
         
         NavigationView {
             
             VStack (alignment: .leading) {
-                
-                Text("ALL Recipes")
+                Text("All Recipes")
                     .bold()
+                    .padding(.top, 40)
                     .font(.largeTitle)
-                  
-                    .padding(.top,40)
                 
-                //.bold() als deze hier staat dan krijgen we een versie error
                 ScrollView {
-                    
-                    // lazyVstack rendert alleen de daadwerkelijke zichtbare scroll items beter voor de CPU
-                    LazyVStack(alignment: .leading) {
+                    LazyVStack (alignment: .leading) {
                         ForEach(vModel.pubRecipes) { r in
                             
-                            NavigationLink(destination:  RecipeDetailView(newVarRecipe: r),
-                                           
-                                           label: {
-                                HStack {
+                            NavigationLink(
+                                destination: RecipeDetailView(newVarRecipe:r),
+                                label: {
                                     
-                                    Image(r.image)
-                                        .resizable()
-                                        .scaledToFill()
-                                        .frame(width: 50, height: 50)
-                                        .clipped()
-                                        .cornerRadius(5)
-                                        .font(.title3)
+                                    // MARK: Row item
+                                    HStack(spacing: 20.0) {
+                                        Image(r.image)
+                                            .resizable()
+                                            .scaledToFill()
+                                            .frame(width: 50, height: 50, alignment: .center)
+                                            .clipped()
+                                            .cornerRadius(5)
+                                        Text(r.name)
+                                            .foregroundColor(.black)
+                                    }
                                     
-                                    Text(r.name)
-                                        .padding()
-                                    
-                                }
-                                .navigationBarHidden(true)
-                                
-                            })
+                                })
                             
                             
                             
                         }
                     }
                 }
-            } .padding()
-            
+                
+                
+            }
+            .navigationBarHidden(true)
+            .padding(.leading)
         }
-        
-        .navigationBarHidden(true)
-        
-        
-        
-        
-        
-        
-        
     }
-    
 }
 
-
-struct RecipeViewList_Previews: PreviewProvider {
+struct RecipeListView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeListView()
             .environmentObject(RecipeViewModel())
     }
 }
+
+
+
+
+
+
+
+
+
+
+// vorige
+//////
+////  RecipeListView.swift
+////  RECIPE_LIST_DEZE
+////
+////  Created by Koen Sas on 08/10/2022.
+////
+//
+//import SwiftUI
+//
+//struct RecipeListView: View {
+//
+//    // opgelet reference naar RecipeViewModel zonder ()
+//    @EnvironmentObject var vModel : RecipeViewModel
+//
+//    var body: some View {
+//
+//
+//
+//
+//        NavigationView {
+//
+//            VStack (alignment: .leading) {
+//
+//                Text("ALL Recipes")
+//                    .bold()
+//                    .font(.largeTitle)
+//
+//                    .padding(.top,40)
+//
+//                //.bold() als deze hier staat dan krijgen we een versie error
+//                ScrollView {
+//
+//                    // lazyVstack rendert alleen de daadwerkelijke zichtbare scroll items beter voor de CPU
+//                    LazyVStack(alignment: .leading) {
+//                        ForEach(vModel.pubRecipes) { r in
+//
+//                            NavigationLink(destination:  RecipeDetailView(newVarRecipe: r),
+//
+//                                           label: {
+//                                HStack {
+//
+//                                    Image(r.image)
+//                                        .resizable()
+//                                        .scaledToFill()
+//                                        .frame(width: 50, height: 50)
+//                                        .clipped()
+//                                        .cornerRadius(5)
+//                                        .font(.title3)
+//
+//                                    Text(r.name)
+//                                        .padding()
+//
+//                                }
+//                                .navigationBarHidden(true)
+//
+//                            })
+//
+//
+//
+//                        }
+//                    }
+//                }
+//            } .padding()
+//
+//        }
+//
+//        .navigationBarHidden(true)
+//
+//
+//
+//
+//
+//
+//
+//    }
+//
+//}
+//
+//
+//struct RecipeViewList_Previews: PreviewProvider {
+//    static var previews: some View {
+//        RecipeListView()
+//            .environmentObject(RecipeViewModel())
+//    }
+//}
